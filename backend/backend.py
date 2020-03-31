1f51f18696cf4c65eeeeead2bf38793e92520964
alirionx <dquilitzsch@outlook.de>
2020-03-31 02:41:01 +0200

    jetzt gehder

---
+++ backend/backend.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import json
+
+from flask import Flask, request, redirect, url_for, send_from_directory, session, render_template
+app = Flask(__name__)
+app.secret_key = "changeit"
+app.debug = True
+
+#-----------------------------
+
+scriptDir = os.path.dirname(os.path.realpath(__file__))
+
+#-----------------------------
+
+#@app.before_request
+#def init_sessions():
+    #session['workdir'] = False
+#    session.permanent = True
+
+#-------------------------------------------------------------------------
+
+@app.route('/', methods=['GET']) 
+def root():
+    return 'welcome to the tables backend api'
+
+
+@app.route('/api/<table>', methods=['GET']) 
+def table_get(table):
+
+    tableObj = {}
+    tableObj["defi"] = []
+    tableObj["data"] = []
+
+    try:
+        defiFileObj = open('defi/'+table+'.json')
+        dataFileObj = open('data/'+table+'.json')
+        
+        defiFileStr = defiFileObj.read()
+        dataFileStr = dataFileObj.read()
+
+        defiObj = json.loads(defiFileStr)
+        dataObj = json.loads(dataFileStr)
+
+    except Exception as e: 
+        print(e)
+        return 'gehderned', 404
+
+    tableObj["defi"] = defiObj
+    tableObj["data"] = dataObj
+
+    jsonOut = json.dumps(tableObj, indent=2)
+
+    #-----------------------------
+    response = app.response_class(
+        response=jsonOut,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+#-------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+#-------------------------------------------------------------------------
--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class Mytable extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.tableList = ["contacts", "cars"];
+    this.menuActions = ["edit", "copy", "delete"];
+    this.state = {
+      table:"",
+      defi:[],
+      data:[],
+      menu: false
+    }
+
+    this.set_table = this.set_table.bind(this);
+    this.table_select = this.table_select.bind(this);
+    this.table_head = this.table_head.bind(this);
+    this.table_body = this.table_body.bind(this);
+    this.action_menu = this.action_menu.bind(this);
+    this.hide_menus = this.hide_menus.bind(this);
+    this.call_action = this.call_action.bind(this);
+  }
+  
+  bg_request(table){
+    if(table !== ""){
+      axios.get('/api/'+table).then(
+        res=> {
+          const tableObj = res.data;
+          console.log(tableObj);
+          this.setState({
+            defi: tableObj.defi,
+            data: tableObj.data,
+            table: table
+          })
+        }
+      )
+    }
+    else{
+      this.setState({
+        defi: [],
+        data: [],
+        table: ""
+      })
+    }
+  }
+
+  componentDidMount(){
+    this.bg_request(this.state.table);
+    document.body.addEventListener('click', (ev) => this.hide_menus(ev.target))
+  }
+
+  set_table(ev){
+    var table = ev.target.value;
+    this.bg_request(table);
+  }
+
+  table_select(){
+    return(
+      <select onChange={this.set_table}>
+        <option value="">please select a table</option>
+        {this.tableList.map( tbl => <option key={tbl} value={tbl}>{tbl}</option> )}
+      </select>
+    )
+  }
+
+  table_head(){
+    return(
+      <thead>
+        <tr>
+          {this.state.defi.map(col => <th key={col.col} style={{textAlign:col.align}}>{col.hl}</th>)}
+          <th>Options</th>
+        </tr>
+      </thead>
+    )
+  }
+
+  table_body(){
+    return(
+      <tbody>
+        {this.state.data.map((row, index) => 
+          <tr key={index} >
+            {this.state.defi.map(col => <td key={col.col + index} style={{textAlign:col.align}}>{row[col.col]}</td>)}
+            <td><this.action_menu rowId={index}/></td>
+          </tr>
+        )}
+      </tbody>
+    )
+  }
+
+
+  call_action(rowId, act){
+    alert('route to: '+this.state.table + ' - ' + rowId + ' - ' + act); 
+    this.setState({menu:false})
+  }  
+
+  action_menu(props){
+    let test ={}
+    if(this.state.menu === props.rowId){
+      return (
+        <div className="ActionMenu" style={{zIndex:2}}>
+        <div>action</div>
+          {this.menuActions.map( act => <div onClick={() => this.call_action(props.rowId, act) } menu="yes" key={act}>{act}</div> )}
+        </div>
+      )
+    }
+    else{
+      return(
+        <div className="ActionMenu" style={test}>
+          <div onClick={()=>this.setState({menu:props.rowId})}>action</div>
+        </div>
+      )
+    }
+  }
+
+  hide_menus(target){
+    if(!target.hasAttribute("menu")){
+      this.setState({menu:false});
+    }
+  }
+
+  render(){
+    let tblElm
+    if(this.state.table!==""){
+      tblElm = <table>
+        <this.table_head />
+        <this.table_body />
+      </table>
+    }  
+    return (
+        <div>
+          <this.table_select />
+            {tblElm}
+        </div>
+      )
+  }
+
 }
 
-export default App;
+
